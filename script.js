063d8c9e5d984e57b88b347caa3e68e55c4fba0c
stoyb <soniatoyb@yahoo.fr>
2023-06-12 11:48:52 +0200

    search with filters

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function recipesWithIngredientsFilter(option, item, listeSuggestions, filterButt
         filterButton.classList.add(filterButtonClass);
         filterButton.textContent = item;
         listFilters.appendChild(filterButton);
-        listeSuggestions.removeChild(option);
+        listeSuggestions.innerHTML = " ";
         listOfRecipes.innerHTML = " ";
         // Affiche le résultat des recherches dans listOfRecipes
         recipes.forEach((recipe) => {
@@ -94,10 +94,56 @@ function recipesWithIngredientsFilter(option, item, listeSuggestions, filterButt
     });
 }
 
+function recipesWithAppliancelFilter(option, item, listeSuggestions, filterButtonClass) {
+    option.addEventListener('click', function() {
+        const filterButton = document.createElement('p');
+        filterButton.classList.add(filterButtonClass);
+        filterButton.textContent = item;
+        listFilters.appendChild(filterButton);
+        listeSuggestions.innerHTML = " ";
+        listOfRecipes.innerHTML = " ";
+        // Affiche le résultat des recherches dans listOfRecipes
+        recipes.forEach((recipe) => {
+            if (recipe.appliance.toLowerCase() === item.toLowerCase()) {
+                const modelCard = recipeFactory(recipe);
+                const cardRecipe = modelCard.getRecipeCardDOM();
+                listOfRecipes.appendChild(cardRecipe);
+            }
+           
+        });
+    });
+}
+
+function recipesWithUtensilslFilter(option, item, listeSuggestions, filterButtonClass) {
+    option.addEventListener('click', function() {
+        const filterButton = document.createElement('p');
+        filterButton.classList.add(filterButtonClass);
+        filterButton.textContent = item;
+        listFilters.appendChild(filterButton);
+        listeSuggestions.innerHTML = " ";
+        listOfRecipes.innerHTML = " ";
+        // Affiche le résultat des recherches dans listOfRecipes
+        recipes.forEach((recipe) => {
+            recipe.ustensils.forEach((utensil) => {
+            if (utensil.toLowerCase() === item.toLowerCase()) {
+                const modelCard = recipeFactory(recipe);
+                const cardRecipe = modelCard.getRecipeCardDOM();
+                listOfRecipes.appendChild(cardRecipe);
+                
+            }
+            });
+        });
+    });
+}
+
 
 
 function showSuggestionsIngredients() {
     const valeurRecherche = champRechercheIngredients.value.toLowerCase();
+    let dataKeyword = state.keyword;
+    dataKeyword = dataKeyword + valeurRecherche 
+    
+    console.log(dataKeyword);
     listeSuggestionsIngredients.innerHTML = " "; 
     if (valeurRecherche.length == 0 ) {
         let dataListItems = state.ingredients;
@@ -107,11 +153,20 @@ function showSuggestionsIngredients() {
             option.textContent = item;
             listeSuggestionsIngredients.appendChild(option);  
             recipesWithIngredientsFilter(option, item, listeSuggestionsIngredients, 'filter-button__ingredients');
+            recipes.forEach((recipe) => {
+                recipe.ingredients.forEach((ingredients) => {
+                if (ingredients.ingredient.toLowerCase() === item.toLowerCase()) {
+                    const modelCard = recipeFactory(recipe);
+                    const cardRecipe = modelCard.getRecipeCardDOM();
+                    listOfRecipes.appendChild(cardRecipe);
+                }
+                });
+            });
         });
     }
-    if (valeurRecherche.length >= 3) {
+    if (valeurRecherche.length >= 1) {
         const suggestions = state.ingredients.filter(item =>
-            item.toLowerCase().startsWith(valeurRecherche)
+            item.toLowerCase().startsWith(valeurRecherche) || item.toLowerCase().includes(" " + valeurRecherche) || item.toLowerCase().endsWith(valeurRecherche + " ")
             );
         suggestions.forEach(suggestion => {
             const option = document.createElement("li");
@@ -119,6 +174,7 @@ function showSuggestionsIngredients() {
             option.textContent = suggestion;
             listeSuggestionsIngredients.appendChild(option);
             recipesWithIngredientsFilter(option, suggestion, listeSuggestionsIngredients, 'filter-button__ingredients');
+             
             });
     }
 }
@@ -133,19 +189,19 @@ function showSuggestionsAppliance() {
             option.classList.add("li-ingredients");
             option.textContent = item;
             listeSuggestionsAppliance.appendChild(option);  
-            recipesWithIngredientsFilter(option, item, listeSuggestionsAppliance, 'filter-button__appliance');
+            recipesWithAppliancelFilter(option, item, listeSuggestionsAppliance, 'filter-button__appliance');
         });
     }
-    if (valeurRecherche.length >= 3) {
+    if (valeurRecherche.length >= 1) {
         const suggestions = state.appliance.filter(item =>
-            item.toLowerCase().startsWith(valeurRecherche)
+            item.toLowerCase().startsWith(valeurRecherche) || item.toLowerCase().includes(" " + valeurRecherche) || item.toLowerCase().endsWith(valeurRecherche + " ")
             );
         suggestions.forEach(suggestion => {
             const option = document.createElement("li");
             option.classList.add("li-ingredients");
             option.textContent = suggestion;
             listeSuggestionsAppliance.appendChild(option);
-            recipesWithIngredientsFilter(option, suggestion, listeSuggestionsAppliance, 'filter-button__appliance');
+            recipesWithAppliancelFilter(option, suggestion, listeSuggestionsAppliance, 'filter-button__appliance');
             });
     }
 }
@@ -160,19 +216,19 @@ function showSuggestionsUtensils() {
             option.classList.add("li-ingredients");
             option.textContent = item;
             listeSuggestionsUtensils.appendChild(option);  
-            recipesWithIngredientsFilter(option, item, listeSuggestionsUtensils, 'filter-button__utensils');
+            recipesWithUtensilslFilter(option, item, listeSuggestionsUtensils, 'filter-button__utensils');
         });
     }
     if (valeurRecherche.length >= 1) {
         const suggestions = state.utensils.filter(item =>
-            item.toLowerCase().startsWith(valeurRecherche)
+            item.toLowerCase().startsWith(valeurRecherche) || item.toLowerCase().includes(" " + valeurRecherche) || item.toLowerCase().endsWith(valeurRecherche + " ")
             );
         suggestions.forEach(suggestion => {
             const option = document.createElement("li");
             option.classList.add("li-ingredients");
             option.textContent = suggestion;
             listeSuggestionsUtensils.appendChild(option);
-            recipesWithIngredientsFilter(option, suggestion, listeSuggestionsUtensils, 'filter-button__utensils');
+            recipesWithUtensilslFilter(option, suggestion, listeSuggestionsUtensils, 'filter-button__utensils');
             });
     }
 }
@@ -223,15 +279,15 @@ function getRecipes() {
     // const filteredList = filterAllList(keyWords);
     // console.log(filteredList);
   
-    if (valeurRecherche.length === 0) {
+    if (valeurRecherche.length == 0) {
       listOfRecipes.innerHTML = "";
       displayRecipes(recipes);
     } else if (valeurRecherche.length >= 3) {
       const suggestionsName = recipes.filter(item =>
-        item.name.toLowerCase().includes(valeurRecherche) || item.name.toLowerCase().endsWith(valeurRecherche + " ")
+        item.name.toLowerCase().includes(" " + valeurRecherche + " ") || item.name.toLowerCase().endsWith(valeurRecherche + " ")
       );
       const suggestionsDescription = recipes.filter(item =>
-        item.description.toLowerCase().includes(valeurRecherche) || item.description.toLowerCase().includes(valeurRecherche + " ")
+        item.description.toLowerCase().includes(" " + valeurRecherche + " ") || item.description.toLowerCase().endsWith(valeurRecherche + " ")
       );
       const suggestionsIngredients = recipes.filter(recipe =>
         recipe.ingredients.some(ingredient =>
@@ -246,7 +302,7 @@ function getRecipes() {
         ...suggestionsIngredients
       ];
   
-      displayRecipes(allSuggestions);
+      displayRecipes(filterAllList(allSuggestions));
       dataIngredients.splice(0, dataIngredients.length);
       dataIngredients = getUniqueItems(getAllIngredients, dataIngredients, allSuggestions);
       dataAppliance.splice(0, dataAppliance.length);
